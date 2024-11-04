5b3de86d28c66f07cce37876df8300fcd4c8cb82
Alec-Nesat Colak <acolak9@ufl.edu>
2024-11-04 13:02:08 -0500

    updated function

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -1,41 +1,70 @@
+using System;
+using System.IO;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace EsepWebhook;
-
-public class Function
+namespace EsepWebhook
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
-    public string FunctionHandler(object input, ILambdaContext context)
+    public class Function
     {
-        context.Logger.LogInformation($"FunctionHandler received: {input}");
-    
-        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());        
-        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
-        
-        context.Logger.LogInformation($"Body parsed: {body}");
-        
-        string payload = $"{{'text':'Issue Created: {body.issue.html_url}'}}";
-        
-        var client = new HttpClient();
-        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+        private static readonly HttpClient client = new HttpClient();
+
+        /// <summary>
+        /// A function that processes a GitHub Issues webhook event and posts to Slack.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task<string> FunctionHandler(object input, ILambdaContext context)
         {
-            Content = new StringContent(payload, Encoding.UTF8, "application/json")
-        };
-    
-        var response = client.Send(webRequest);
-        using var reader = new StreamReader(response.Content.ReadAsStream());
-            
-        return reader.ReadToEnd();
+            try
+            {
+                context.Logger.LogInformation($"FunctionHandler received: {input}");
+
+                // Deserialize the input JSON
+                dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+                
+                // Extract the issue URL from the JSON payload
+                string issueUrl = json?.issue?.html_url;
+                if (string.IsNullOrEmpty(issueUrl))
+                {
+                    throw new Exception("No issue URL found in the payload.");
+                }
+                
+                // Prepare the Slack message payload
+                string payload = JsonConvert.SerializeObject(new { text = $"Issue Created: {issueUrl}" });
+
+                // Set up the Slack webhook URL from environment variables
+                string slackUrl = Environment.GetEnvironmentVariable("SLACK_URL");
+                if (string.IsNullOrEmpty(slackUrl))
+                {
+                    throw new Exception("SLACK_URL environment variable is not set.");
+                }
+
+                // Create the HTTP request to post to Slack
+                var webRequest = new HttpRequestMessage(HttpMethod.Post, slackUrl)
+                {
+                    Content = new StringContent(payload, Encoding.UTF8, "application/json")
+                };
+
+                // Send the request asynchronously
+                var response = await client.SendAsync(webRequest);
+                response.EnsureSuccessStatusCode();
+
+                // Read and return the response content
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+                context.Logger.LogError($"Error in FunctionHandler: {ex.Message}");
+                throw;
+            }
+        }
     }
 }
